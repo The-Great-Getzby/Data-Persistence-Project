a3f13606ec6b570ccc2b3d947380a6856808906a
The-Great-Getzby <97181760+The-Great-Getzby@users.noreply.github.com>
2022-01-07 17:01:20 -0500

    Implemented data persistence between sessions
    Added code to MainManager script  to implement data persistence between sessions--namely, a high score and highScorePlayerName feature to load these values on entering scene "main", update them if the current player's score exceeds the high score, and save them if the game ends or if it is quit.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,28 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MainManager : MonoBehaviour
 {
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
-
-    public Text ScoreText;
-    public Text PlayerNameText;
     public GameObject GameOverText;
-    //public GameObject MenuUIHandlerScript;
-    
+    public Text ScoreText;// the object that displays the active player's current score
+    public Text HighScoreText; // the object that displays all high score text
+    //Get playerNameInput from other script MenuUIHandler
+
     private bool m_Started = false;
-    private int m_Points;
+    private int m_Points; //track current points
+    private int highScore = 0; //track high score, initialized to zero
+    private string highScorePlayerName = "None"; // track high scorer, initialized to none
     
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
+        //Check to see if there is saved data (done in method)
+        //If there is, Load in High score and High score player name
+        LoadHighScore();
 
+        //Game Logic
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
         
@@ -43,6 +52,7 @@ public class MainManager : MonoBehaviour
 
     private void Update()
     {
+        //Game Logic
         if (!m_Started)
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -58,28 +68,97 @@ public class MainManager : MonoBehaviour
         }
         else if (m_GameOver)
         {
+            //write high score and hig score playername to save
+
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
+
+        //High Score Logic
+
+        //check to see if current score is higher than High Score
+        //if yes, replace high score and high score player name with current score and current player name
+        if (highScore < m_Points)
+        {
+            highScore = m_Points;
+            highScorePlayerName = MenuUIHandler.playerNameInput;
+
+        }
+
+        //display the high score and name. Update if it changes.
+        HighScoreText.text = $"High Score: {highScorePlayerName} : {highScore}";
+
     }
 
+
+    //Quit
+
+    //save data when app is quit
+    private void OnApplicationQuit()
+    {
+        Debug.Log("Application ended");
+        SaveHighScore();
+    }
+
+
+
+    //Methods
+
+    //Scoring
     void AddPoint(int point)
     {
         m_Points += point;
         ScoreText.text = $"Score : {m_Points}";
     }
 
-    void DisplayPlayerName(string s)
-    {
-        s = MenuUIHandler.playerNameInput;
-        PlayerNameText.text = $"Player Name : {MenuUIHandler.playerNameInput}";
-    }
 
+    //Game OVer
     public void GameOver()
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
+        SaveHighScore(); //save high score data upon gameover 
     }
+
+
+    //Saving Data
+
+
+    [System.Serializable] // must be serializeable for JSON use
+    class SaveData // new class for the data we want to save and load
+    {
+        public int dataHighScore; // internal variable for high score
+        public string dataHighScorePlayerName; // internal variable for high score player name
+    }
+
+
+    //Method for saving high score/playername as a JSON file
+    public void SaveHighScore()
+    {
+        SaveData data = new SaveData(); // create a new instance of the SaveData class called "data"
+        data.dataHighScore = highScore; // assign the value of highScore within the object "data" to be equal to the current high score saved in MainManager
+        data.dataHighScorePlayerName = highScorePlayerName; // . . .
+
+
+        string json = JsonUtility.ToJson(data); //transform this instance to a JSON string called "json"
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json); //write the JSON string to a file
+    }
+
+
+    public void LoadHighScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            highScore = data.dataHighScore;
+            highScorePlayerName = data.dataHighScorePlayerName;
+        }
+    }
+
 }
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -7,47 +7,51 @@ using TMPro;
 
 public class MenuUIHandler : MonoBehaviour
 {
-    public Text PlayerNameText;
-    public static string playerNameInput;
-    Scene startScene;
-    Scene currentScene;
+    public Text PlayerNameText; // Text object to display the input playername
+    public static string playerNameInput; // string that the player inputs
+    Scene currentScene; // scene variable used to get the current scene
 
     // Start is called before the first frame update
     void Start()
     {
-        //PlayerNameText = Text.FindObjectOfType<Text>();
-        //PlayerNameText.text = "Test";
-        DontDestroyOnLoad(gameObject);
-        //startScene = SceneManager.GetActiveScene();
-
+        //Dont destroy Canvas, the gameObject this is attached to. Only works on root objects, not children
+        DontDestroyOnLoad(gameObject); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Get the current scene
         currentScene = SceneManager.GetActiveScene();
+
+        //check if the current scene is Menu. If it's not, destory the button and input field
         if (currentScene != SceneManager.GetSceneByName("Menu"))
         {
             Destroy(GameObject.Find("Start Button"));
             Destroy(GameObject.Find("Name Input"));
 
+            //attempted to move Player Name object once in the new scene, but it had bugs
             //PlayerNameText.transform.position = new Vector2(1, 1);
         }
     }
 
-    public void StartNew() // new custom method
+
+    // Methods
+
+
+    public void StartNew() // Assigned to start button, clikc to leave Menu and enter main
     {
         SceneManager.LoadScene(1);
     }
 
-    public void ReadStringInput(string s)
+    public void ReadStringInput(string s) // assigned to text field, get the player's input text
     {
         playerNameInput = s;
-        Debug.Log(playerNameInput);
+        Debug.Log(playerNameInput); // show input name in log
         SetPlayerName(playerNameInput);
     }
 
-    public void SetPlayerName (string playerNameInput)
+    public void SetPlayerName (string playerNameInput) // the text of the "PlayerNameText" Text object will include the player's input 
     {
         PlayerNameText.text = "Player Name: " + playerNameInput;
     }
